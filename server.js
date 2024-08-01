534a8a083457255fe12c19378336620460c1e574
Avineet Yadav <12minute00@gmail.com>
2024-08-01 20:45:12 +0530

    add

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const io = new Server(server, {
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [process.env.FRONTEND_URI],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
