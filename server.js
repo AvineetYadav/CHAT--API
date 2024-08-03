c21508a611bd72dd4a0d21bcb4e1b5bdbb6f3bb4
Avineet Yadav <12minute00@gmail.com>
2024-08-03 19:08:40 +0530

    cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ config({
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL], 
+    origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -24,7 +24,7 @@ app.use(
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.FRONTEND_URL],
+    origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
