9b3b7bb07f55f74a0c23d12562dd96e2c28cc5cc
Avineet Yadav <12minute00@gmail.com>
2024-08-03 19:31:46 +0530

    cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(`/api/users`, userRouter);
 
 app.use(
   cors({
-    origin: '*',
+    origin:  [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
